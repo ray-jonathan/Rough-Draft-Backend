88109c8d7664a8b32821307e191cd00326c0d9f1
Jonathan Ray <jonathanray17@gmail.com>
2019-10-30 12:16:22 -0400

    adds sql data for all states in table

--- schema.sql
+++ schema.sql
@@ -1,9 +1,9 @@
 create type us_state as enum ('AL','AK','AZ','AR','CA','CO','CT','DC','DE','FL','GA','HI','ID','IL','IN','IA','KS','KY','LA','ME','MD','MA','MI','MN','MS','MO','MT','NE','NV','NH','NJ','NM','NY','NC','ND','OH','OK','OR','PA','RI','SC','SD','TN','TX','UT','VT','VA','WA','WV','WI','WY');
 
 create table us_states (
-    id serial primary key,
-    us_state us_state,
-    full_name varchar (15),
+    -- id serial primary key,
+    us_state us_state primary key,
+    full_name varchar (25),
     neighboring_states us_state[]
 );
 
@@ -13,7 +13,7 @@ create table breweries (
     brewery_type varchar (25),
     street varchar (100),
     city varchar (50),
-    us_state us_state,
+    us_state us_state references us_states(us_state),
     postal_code varchar (15),
     country varchar (20),
     latitude float,
